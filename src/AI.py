57ef5b246e369d24b4eee2619c246fa4d6e358ee
chema07 <chema071518@gmail.com>
2014-06-02 03:17:37 +0200

    Partially implemented AI event notification scheme

--- src/AI.py
+++ src/AI.py
@@ -43,18 +43,19 @@ class A_star:
 
 class Think(multiprocessing.Process):
     #The following class code is deprecated (thread oriented ...)
-    def __init__(self, initial_state, depth):
-        threading.Thread.__init__(self)
+    def __init__(self, agent_conn, child_conn, initial_state, depth):
+        multiprocessing.Process.__init__(self)
         self.level1 = A_star()
         self.level2 = AB(initial_state, depth)
-        self.ready = threading.Event()
-        self.access_lock = threading.Lock()
+        self.agent_conn = agent_conn
+        #self.ready = threading.Event()
+        #self.access_lock = threading.Lock()
 
 
-    def _reveal_plan(self, plan):
-        self.access_lock.acquire()
-        self.plan = plan
-        self.access_lock.release()
+    def send_plan(self):
+        #self.access_lock.acquire()
+        self.agent_conn.send(self.plan)
+        #self.access_lock.release()
 
 
     def run(self):
@@ -87,8 +88,8 @@ class Agent:
     agent server. When a response is available, it makes the agent assets
     execute their AI core commands.
     """
-    def __init__(self):
-        pass
+    def __init__(self, think_conn):
+        self.think_conn = think_conn
 
     def addAsset(self, asset):
         """
@@ -120,7 +121,8 @@ class Agent:
         """
         Executes next command of the plan (if previous is complete).
         """
-        pass
+        if self.think_conn.poll():
+            self.plan = self.think_conn.recv()
 
 
 class AgentServer:
@@ -128,21 +130,38 @@ class AgentServer:
     This class holds a common processed view of the world where
     agents can act through their assets. It begins based on 
     initial level representation and evolves through events.
+    This class is a singleton, use get() class method to retrieve
+    the instance.
     """
+    instance = None
+    
+    @classmethod
+    def get(cls):
+        if cls.instance is None:
+            cls.instance = cls()
+        return cls.instance
+    
     def __init__(self):
-        pass
+        self.core_list = []
+      
 
     def newAgent(self):
         """
         Creates a new agent and prepares data structures.
         """
-        pass
+        think_conn, agent_conn = multiprocessing.Pipe()
+        parent_conn, child_conn = multiprocessing.Pipe()
+        agent = Agent(think_conn)
+        think = Think(agent_conn, child_conn)
+        self.core_list.append((agent, think, parent_conn))
+        return agent
 
     def startAll(self):
         """
         Order the execution of all AI cores registered in the server.
         """
-        pass
+        for core in self.core_list:
+            core[1].start()
 
     def stopAll(self):
         """
@@ -150,4 +169,62 @@ class AgentServer:
         """
         pass
 
+    def moveEast(self):
+        """
+        moveEast() -> Notifies asset has moved east one tile
+        """
+        pass
+
+    def moveWest(self):
+        """
+        moveWest() -> Notifies asset has moved west one tile
+        """
+        pass
+
+    def moveNorth(self):
+        """
+        moveNorth() -> Notifies asset has moved north one tile
+        """
+        pass
+
+    def moveSouth(self):
+        """
+        moveSouth() -> Notifies asset has moved south one tile
+        """
+        pass
+        
+
+    def changeGun(self):
+        """
+        Method that notifies that asset has changed guns
+        changeGun() -> switches to the next gun, so if the automatic gun
+        was the current gun, then the shotgun is now the current gun and
+        vice-versa
+        """
+        pass
+
+    def fireWest(self):
+        """
+        Method that notifies that asset has fired the current gun to the left
+        """
+        pass
+
+    def fireNorth(self):
+        """
+        Method that notifies that asset has fired the current gun up
+        """
+        pass
+
+    def fireEast(self):
+        """
+        Method that notifies that asset has fired the current gun east
+        """
+        pass
+
+    def fireSouth(self):
+        """
+        Method that notifies that asset has fired the current gun down
+        """
+        pass       
+        
 
